3ef7e75ccdbb8c1d8ac23bc80d035aa126488349
1001-1110 <joshuacapulemanzano@gmail.com>
2018-07-19 20:22:07 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+
+public class Main {
+
+	//Train
+	public void station_load_train(Station station, int count) {
+		
+	}	
+	
+	//Passenger
+	public void station_wait_for_train(Station station) {
+		
+	}
+	
+	//Passenger
+	public void station_on_board(Station station) {
+		
+	}
+	
+	//Start
+	public void station_init() {
+		
+	}
+	
+	public void lock_init(Lock lock) {
+		
+	}
+	
+	public void lock_acquire(Lock lock) {
+		
+	}
+	
+	public void lock_release(Lock lock) {
+		
+	}
+	
+	public void cond_init(Condition cond) {
+		
+	}
+	
+	public void cond_wait(Condition cond, Lock lock) {
+		
+	}
+	
+	public void cond_signal(Condition cond, Lock lock) {
+		
+	}
+	
+	public void cond_broadcast(Condition cond, Lock lock) {
+		
+	}
+	
+}
